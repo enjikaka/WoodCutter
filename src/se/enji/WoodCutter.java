a0b5d95d54177c517be7eaf9cfec691f8a10ebea
Jeremy Karlsson <jeremy@enji.se>
2014-10-25 21:12:28 +0200

    v1.4.6

--- src/se/enji/WoodCutter.java
+++ src/se/enji/WoodCutter.java
@@ -1,8 +1,12 @@
 package se.enji;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,50 +14,48 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WoodCutter extends JavaPlugin implements Listener {
+	FileConfiguration config;
+	List<?> breakable = Arrays.asList(new Material[]{Material.LOG, Material.LOG_2});
+	List<?> surroundable = Arrays.asList(new Material[]{Material.LOG, Material.LOG_2, Material.DIRT, Material.GRASS});
+
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		config = getConfig();
+		config.options().copyDefaults(true);
+		saveConfig();
 	}
-	
+
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
 		Player p = e.getPlayer();
-		if (!acceptsPlayer(p)) return;
-		Block b = e.getBlock();
-		if (!isAxe(p.getItemInHand().getType())) return;
-		if (b.getType()!=Material.LOG) return;
-		int x=b.getX();
-		int y=b.getY();
-		int z=b.getZ();
-		if (b.getWorld().getBlockAt(x,(y-1),z).getType()!=Material.DIRT) return;
-		World w = b.getWorld();
-		boolean logsLeft=true;
-		int fallen=0;
+		if (!p.hasPermission("woodcutter.use")) return;
+		if (!breakable.contains(e.getBlock().getType()) || !isAxe(p.getItemInHand().getType())) return;
+		Location l = e.getBlock().getLocation();
+		if (!surroundable.contains(l.subtract(0.0, 1.0, 0.0).getBlock().getType()) || !surroundable.contains(l.add(0.0, 1.0, 0.0).getBlock().getType())) return;
+		columnRemove(l, p);
+	}
+
+	private void columnRemove(Location location, Player player) {
+		boolean logsLeft = true;
+		int fallen = 0;
+		location.subtract(0.0, 1.0, 0.0);
 		while (logsLeft) {
-			y++;
-			Block d=w.getBlockAt(x,y,z);
-			if (d.getType()==Material.LOG) {
-				remove(d);
+			Block block = location.add(0.0,1.0,0.0).getBlock();
+			if (breakable.contains(block.getType())) {
+				block.breakNaturally();
 				fallen++;
 			}
-			else logsLeft=false;
+			else logsLeft = false;
+
+			Location newLocation = block.getLocation().subtract(1.0, 0, 0);if (breakable.contains(newLocation.getBlock().getType())) columnRemove(newLocation, player);
+			newLocation = block.getLocation().subtract(0, 0, 1.0);if (breakable.contains(newLocation.getBlock().getType())) columnRemove(newLocation, player);
+			newLocation = block.getLocation().subtract(0, 0, -1.0);if (breakable.contains(newLocation.getBlock().getType())) columnRemove(newLocation, player);
+			newLocation = block.getLocation().subtract(-1.0, 0, 0);if (breakable.contains(newLocation.getBlock().getType())) columnRemove(newLocation, player);
 		}
-		p.getItemInHand().setDurability((short)(p.getItemInHand().getDurability()+fallen));
+		player.getItemInHand().setDurability((short)(player.getItemInHand().getDurability() + fallen));
 	}
-	
+
 	private boolean isAxe(Material a) {
-		if (a==Material.IRON_AXE) return true;
-		if (a==Material.WOOD_AXE) return true;
-		if (a==Material.GOLD_AXE) return true;
-		if (a==Material.DIAMOND_AXE) return true;
-		if (a==Material.STONE_AXE) return true;
-	    return false;
-	}
-	
-	private boolean acceptsPlayer(Player p) {
-		return p.hasPermission("woodcutter.use");
-	}
-	
-	public void remove(Block b) {
-		if (b.getType()==Material.LOG||b.getType()==Material.LEAVES) b.breakNaturally();
+		return !config.getBoolean("needAxe") ? true : a.toString().endsWith("_AXE");
 	}
 }

0e0b8356ec1887bbc4c7c7569489f8c3e9f1bf26
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-11-23 23:52:32 +0000

    Fixed WoodCutter bypassing WorldGuard by making use of monitor event priority and ignoring cancelled events

--- src/se/enji/WoodCutter.java
+++ src/se/enji/WoodCutter.java
@@ -9,6 +9,7 @@ import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
@@ -35,7 +36,7 @@ public class WoodCutter extends JavaPlugin implements Listener {
 		saveConfig();
 	}
 
-	@EventHandler
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
 	public void onBlockBreak(BlockBreakEvent e) {
 		Player p = e.getPlayer();
 		
